00c4ee7e176fd45081c74da0c36a153c3be5af91
abugi <abugiboi@gmail.com>
2022-04-07 16:41:46 +0100

    feat: place random points

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,52 @@
 <template>
-  <div></div>
+  <div id="map" ref="map">reo test</div>
 </template>
 
 <script>
 export default {
   name: 'App',
-  async mounted() {
-    this.google = window.google
-    this.map = window.createMap
-
-    if (this.google) {
-      this.addMarker()
-    }
-  },
   data() {
     return {
-      google: null,
       map: null,
+      center: { lat: 9.0747, lng: 7.476 },
     }
   },
+  mounted() {
+    this.addMap()
+    this.addMarker(this.center)
+    this.addMarker({ lat: 9.5836, lng: 6.5463 })
+
+    this.placePoint()
+  },
   methods: {
-    async addMarker() {
-      await new this.google.maps.Marker({
-        position: { lat: 9.0787, lng: 7.4702 },
-        map: this.map(),
+    getMap(callback) {
+      let vm = this
+      function checkForMap() {
+        if (vm.map) {
+          callback(vm.map)
+        } else {
+          setTimeout(checkForMap, 200)
+        }
+      }
+
+      checkForMap()
+    },
+    addMap() {
+      this.map = new window.google.maps.Map(this.$refs['map'], {
+        center: this.center,
+        zoom: 8,
       })
-
-      // const infoWindow = await new this.google.maps.InfoWindow({
-      //   content: '<h1>Wuse 2, Abuja</h1>',
-      // })
-
-      // marker.addListener('click', function () {
-      //   infoWindow.open(this.map, marker)
-      // })
+    },
+    addMarker(coords) {
+      new window.google.maps.Marker({
+        position: coords,
+        map: this.map,
+      })
+    },
+    placePoint() {
+      window.google.maps.event.addListener(this.map, 'click', (event) => {
+        this.addMarker({ lat: event.latLng.lat(), lng: event.latLng.lng() })
+      })
     },
   },
 }
@@ -50,6 +64,7 @@ export default {
 
 #map {
   width: 100%;
-  height: 600px;
+  height: 450px;
+  background: #eee;
 }
 </style>
